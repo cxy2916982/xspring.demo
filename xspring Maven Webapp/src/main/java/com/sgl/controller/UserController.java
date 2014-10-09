405bc4708fd0a04ee8573e3aaac666809fee2571
cxy2916982 <chenxiang11@126.com>
2014-10-09 09:49:18 +0800

    initial check in of xspring

---
+++ xspring Maven Webapp/src/main/java/com/sgl/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.sgl.controller;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sgl.model.User;
+import com.sgl.service.UserService;
+
+@Controller
+public class UserController {
+	@Autowired
+	private UserService userService;
+	@RequestMapping("/reg")
+	public String register(){
+		return "index";
+	}
+	@RequestMapping("/user")
+	public ModelAndView addUser(String username,String password) {
+		
+		ModelAndView mav=null;
+		User user = new User() ;
+		user.setUsername(username) ;
+		user.setPassword(password) ;
+		user.setId(UUID.randomUUID().toString());
+		user.setRegtime(new Date());
+		try {
+			userService.addUser(user);
+		//	request.setAttribute("user", user);
+			mav = new ModelAndView();
+			mav.setViewName("success");
+			mav.addObject("user", user);
+			mav.addObject("msg", "注册成功了，可以去登陆了");
+			return mav;
+		} catch (Exception e) {
+			mav.setViewName("error");
+			mav.addObject("user", null);
+			mav.addObject("msg", "注册失败");
+			return mav;
+		}
+	}
+
+}
---
+++ xspring Maven Webapp/src/main/java/com/sgl/dao/UserDaoI.java
@@ -0,0 +1,9 @@
+
+package com.sgl.dao;  
+
+import java.io.Serializable;  
+  
+public interface UserDaoI<T>  
+{  
+    public Serializable save(T o);  
+}  
